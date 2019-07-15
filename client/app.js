7893367fdc699e04059de384a6f033a091a5d7f4
unknown <pawelstepien1000@gmail.com>
2019-07-15 23:30:46 +0200

    Starting phaser setup

--- client/app.js
+++ client/app.js
@@ -12,6 +12,53 @@ form.addEventListener('submit', event => {
     sendLoginFormData();
 });
 
+class PhaserApi {
+    constructor() {
+        this.phaser = new Phaser.Game(32*17, 32*17, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+
+        function preload () {
+            this.load.setBaseURL();
+            this.load.image('grass', 'assets/sprites/tiles/grass.png');
+            this.load.image('lava', 'assets/sprites/tiles/lava.png');
+            this.load.image('mage', 'assets/sprites/characters/mage.png');
+        }
+        
+        function create () {
+            const player = phaser.add.image(0, 0, 'mage');
+            this.phaser.camera.follow(player);
+        }
+
+        function update () {
+            
+        }
+
+    }
+    drawState(state) {
+        for (let x = 0; x < state.length; x++) {
+            for (let y = 0; y < state[x].length; y++) {
+                if (!state[x][y]) {
+                    if (typeof state[x][y] === 'undefined') {
+                        this.phaser.add.image(x * 32, y * 32, 'lava');
+                    } else if (state[x][y] === null) {
+                        this.phaser.add.image(x * 32, y * 32, 'lava');
+                    }
+                    continue; 
+                }
+
+                this.phaser.add.image(x * 32, y * 32, 'grass');
+                const player = state[x][y].player;
+                if (player !== null/* && player.id !== this.playerId*/) {
+                    this.phaser.add.image(x * 32, y * 32, 'mage');
+                }
+            }
+        }
+    }
+
+        
+        
+    
+}
+
 class Game {
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
@@ -20,6 +67,7 @@ class Game {
         this.canvasWidth = canvas.width;
         this.canvasHeight = canvas.height;
         this.playerId = '';
+        this.phaserApi = new PhaserApi();
     }
     start() {
         socket.on('player id', playerId => {
@@ -30,7 +78,9 @@ class Game {
             console.log('credential rejected')
         });
         socket.on('state update', state => {
-            this.drawState(JSON.parse(state));
+            // this.drawState(JSON.parse(state));
+            // this.drawStatePhaser(JSON.parse(state));
+            this.phaserApi.drawState(JSON.parse(state));
         });
         this.initArrows();
     }
@@ -52,6 +102,29 @@ class Game {
                 const player = this.state[x][y].player;
                 if (player !== null && player.id !== this.playerId) {
                     this.drawPlayer(x, y);
+                }
+            }
+        }
+        this.drawClientPlayer();
+    }
+    drawStatePhaser(state) {
+        this.state = state;
+        // this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+        for (let x = 0; x < this.state.length; x++) {
+            for (let y = 0; y < this.state[x].length; y++) {
+                if (!this.state[x][y]) {
+                    if (typeof this.state[x][y] === 'undefined') {
+                        phaser.add.image(x * 32, y * 32, 'lava');
+                    } else if (this.state[x][y] === null) {
+                        phaser.add.image(x * 32, y * 32, 'lava');
+                    }
+                    continue; 
+                }
+
+                phaser.add.image(x * 32, y * 32, 'grass');
+                const player = this.state[x][y].player;
+                if (player !== null && player.id !== this.playerId) {
+                    phaser.add.image(x * 32, y * 32, 'mage');
                 }
             }
         }
@@ -100,3 +173,5 @@ class Game {
 const canvas = document.getElementById('canvas');
 const game = new Game(canvas);
 game.start();
+
+
